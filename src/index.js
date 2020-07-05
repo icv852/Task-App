5a4fb8de32cc7d22835f4d291a412b638bced661
icv852 <icv852@gmail.com>
2020-07-05 12:23:03 +0800

    Adding Support for the File Uploads

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')  
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 
 app.use(express.json())
@@ -19,19 +22,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-
-const main = async () => {
-    // const task = await Task.findById('5efcb4ca04cf4668accb3f48')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5efd5f3093bae4644032a76b')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
